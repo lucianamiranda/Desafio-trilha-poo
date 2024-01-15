ff384702af5c7d0535f339806244e89ddda3e0cd
lucianamiranda <109171760+lucianamiranda@users.noreply.github.com>
2024-01-14 21:53:27 -0300

    Add files via upload
    Desafio concluído

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia("123", "Nokia Modelo", "123456789", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Iphone iphone = new Iphone("456", "iPhone Modelo", "987654321", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+ iphone.InstalarAplicativo("Instagram");
+
+
